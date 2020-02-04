ff7b906983dcfe0880e98310346dcd4b4e1d0404
stefandebruyn <stefandebruyn@utexas.edu>
2020-02-04 09:53:59 -0600

    Add telemetry decoding to CSV

--- sac2020_lib/telem_decode.cpp
+++ sac2020_lib/telem_decode.cpp
@@ -5,8 +5,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 #include "sac2020_state.h"
+
+static char record[1024];
 
 char* state_name(uint8_t state)
 {
@@ -25,12 +28,20 @@ char* state_name(uint8_t state)
 
 int main(int ac, char** av)
 {
-    FILE* fptr = NULL;
-    fptr = fopen(av[1], "rb");
+    FILE* p_fin = NULL;
+    p_fin = fopen(av[1], "rb");
+
+    FILE* p_fout = NULL;
+    std::string fout_name = std::string(av[1]) + ".csv";
+    p_fout = fopen(fout_name.c_str(), "w");
+    fputs("Time,State,Filtered Altitude,Filtered Velocity,FilteredAcceleration,"
+          "Pressure,Temperature,Barometer Altitude,IMU Temperature,"
+          "Accel X,Accel Y,Accel Z,Accel Vertical,Gyro X,Gyro Y,Gyro Z\n",
+          p_fout);
 
     MainStateVector_t vec;
     uint32_t packet_count = 0;
-    while (fread(&vec, sizeof(vec), 1, fptr) > 0)
+    while (fread(&vec, sizeof(vec), 1, p_fin) > 0)
     {
         packet_count++;
 
@@ -49,9 +60,20 @@ int main(int ac, char** av)
         printf("r=%09.2f p=%09.2f y=%09.2f",
                vec.gyro_r, vec.gyro_p, vec.gyro_y);
 
+        // Write to CSV.
+        sprintf(record, "%.4f,%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%d,%.4f,%.4f,"
+                        "%.4f,%.4f,%.4f,%.4f,%.4f\n",
+                        vec.time, state_name(vec.state), vec.altitude,
+                        vec.velocity, vec.acceleration, vec.pressure,
+                        vec.temperature, vec.baro_altitude, vec.imu_temp,
+                        vec.accel_x, vec.accel_y, vec.accel_z,
+                        vec.accel_vertical, vec.gyro_r, vec.gyro_p, vec.gyro_y);
+        fputs(record, p_fout);
+
         printf("\n");
     }
 
-    fclose(fptr);
+    fclose(p_fin);
+    fclose(p_fout);
     printf("Decoded %d telemetry packets\n", packet_count);
 }
